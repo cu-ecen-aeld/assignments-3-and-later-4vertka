6ab88a6de0a26782575174f5ba3345e1999830de
Oleksandr <sashapisarchuk@gmail.com>
2026-05-26 15:27:37 +0300

    assigment 3 part 1 completed

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,8 @@
 #include "systemcalls.h"
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -17,7 +20,9 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+	if (system(cmd) == 0) {
+		return true;
+	}else {return false;}
 }
 
 /**
@@ -58,6 +63,19 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	pid_t pid = fork();
+	
+	if (pid == -1) {
+		return false;
+	}else if (pid == 0) {
+		execv(command[0], command);
+		exit(EXIT_FAILURE);
+	}else {
+		int status;
+		waitpid(pid, &status, 0);
+		va_end(args);
+		return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+	}
 
     va_end(args);
 
@@ -92,6 +110,24 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+	pid_t kidpid;
+	int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+
+	if (fd < 0) {return false;}
+	switch (kidpid = fork()) {
+		case -1: return false;
+		case 0:
+				 if (dup2(fd, 1) < 0) {exit(EXIT_FAILURE);}
+				 close(fd);
+				 execv(command[0], command);
+				 exit(EXIT_FAILURE);
+		default:
+				 close(fd);
+				 int status;
+				 waitpid(kidpid, &status, 0);
+				 va_end(args);
+				 return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+	}
 
     va_end(args);
 
